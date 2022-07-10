1eab4e6f35723f4e131c148d9ec6573562b247fe
成龙生 <1536463948@qq.com>
2022-07-10 15:31:11 +0800

    测试spring框架小demo

---
+++ spring5_demo1/src/person/cls/User.java
@@ -0,0 +1,9 @@
+package person.cls;
+
+public class User {
+
+    public void add() {
+        // write your code here
+        System.out.println("add...");
+    }
+}
---
+++ spring5_demo1/src/person/cls/test/TestAdd.java
@@ -0,0 +1,12 @@
+package person.cls.test;
+
+import org.junit.Test;
+
+public class TestAdd {
+
+    @Test
+    public void testAdd() {
+        //
+    }
+
+}
