ddf4a94781d88ba40a7981a94d07564c12dd047e
成龙生 <1536463948@qq.com>
2022-07-10 15:35:43 +0800

    测试spring框架小demo

--- spring5_demo1/src/person/cls/test/TestAdd.java
+++ spring5_demo1/src/person/cls/test/TestAdd.java
@@ -1,12 +1,19 @@
 package person.cls.test;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import person.cls.User;
 
 public class TestAdd {
 
     @Test
     public void testAdd() {
-        //
+        // 读取配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        User user = context.getBean("user", User.class);
+        user.add();
+        System.out.println("user = " + user);
     }
 
 }
